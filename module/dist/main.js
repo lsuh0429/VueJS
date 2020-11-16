c93c532c76e74a8828b37d1f211f12d2bed851b0
lsuh0429 <lsuh0429@naver.com>
2020-11-16 09:07:56 +0900

    ..

---
+++ module/dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log(28.26)})();
---
+++ module/src/index.js
@@ -0,0 +1,11 @@
+/**
+ * 1. 원의 넓이를 구하는 공식
+ * 2. PI 정의
+ * 3. 공식을 통해 결과 얻기
+ */
+
+//  const {getCircleArea} = require('./mathUtil');
+import mathUtil from './mathUtil';
+
+ const result = mathUtil.getCircleArea(3);
+ console.log(result);
