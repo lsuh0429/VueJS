add5bcab59394c5a9a1aa393c7bedd41133be39b
lsuh0429 <lsuh0429@naver.com>
2021-01-21 08:40:12 +0900

    story book component ui tool

--- study/src/views/chart/VueChartKick.vue
+++ study/src/views/chart/VueChartKick.vue
@@ -39,6 +39,12 @@
               return {
                   chartData: [['Jan', 44], ['Feb', 27], ['Mar', 60], ['Apr', 55], ['May', 37], ['Jun', 40], ['Jul', 69], ['Aug', 33], ['Sept', 76], ['Oct', 90], ['Nov', 34], ['Dec', 22]]
             }
+        },
+        mounted() {
+            const obj = { title: 'test', name: 'Ho'} 
+            console.log(obj)
+            obj.age = 15
+            console.log(obj)
         }
     }
 </script>
